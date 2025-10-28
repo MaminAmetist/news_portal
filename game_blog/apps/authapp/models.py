107277b9619b24533c106ed66cfa101931158837
Anna <anna.nezdulkina@mail.ru>
2025-10-29 00:07:52 +0700

    creation of utility programs

--- game_blog/apps/authapp/models.py
+++ game_blog/apps/authapp/models.py
@@ -3,8 +3,13 @@ from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType, UUIDType
 from uuid import uuid4
 import datetime
+from authlib.jose import JsonWebSignature
+import json
+import time
 
+from apps.postapp.models import Post
 from db.base_class import Base
+from core.config import SECRET_KEY
 
 
 class User(Base):
@@ -16,3 +21,32 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     post = relationship("Post", back_populates="owner")
     token = Column(String(64), nullable=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        jws = JsonWebSignature(['HS256'])
+        protected = {'alg': 'HS256'}
+        payload = json.dumps({'expires_sec': expires_sec,
+                              'time_sending': time.time(),
+                              'user_uid': str(self.user_id)}).encode('utf-8')
+        secret = SECRET_KEY
+        return jws.serialize_compact(protected, payload, secret).decode('utf-8')
+
+    @staticmethod
+    def get_payload_from_reset_token(token):
+        jws = JsonWebSignature(['HS256'])
+        data = jws.deserialize_compact(token, SECRET_KEY)
+        payload_json = json.loads(data['payload'])
+        time_left = payload_json['time_sending'] + payload_json['expires_sec'] - time.time()
+        print(time_left)
+        if time_left < 0:
+            return False
+        else:
+            return payload_json
+
+
+class Token(Base):
+    token_id = Column(UUIDType, default=uuid4, primary_key=True)
+    token = Column(UUIDType, unique=True, nullable=False, index=True,
+                   default=uuid4)
+    expires = Column(DateTime())
+    user_uid = Column(UUIDType, ForeignKey('user.user_id'))
